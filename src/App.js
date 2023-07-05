8db0aabb689b2dafc98a809d413a58ccc2c56335
Balamurugan-vk <138617247+Balamurugan-vk@users.noreply.github.com>
2023-07-05 13:33:19 +0530

    deplloyment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import LandingPage from './LandingPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import Boat from './Boat';
+import FireBolt from './FireBolt';
+import Noise from './Noise';
+import Gizmore from './Gizmore';
+// import LandingPage from './LandingPage';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {/* <BrowserRouter> */}
+      <Routes>
+        <Route path='/' element={ <LandingPage /> } />
+        <Route path='/home' element={ <Home /> } />
+        <Route path='/boat' element={ <Boat /> } />
+        <Route path='/firebolt' element={ <FireBolt /> } />
+        <Route path='/noise' element={ <Noise /> } />
+        <Route path='/gizmore' element={ <Gizmore /> } />
+        <Route path='/landingpage' element={ <LandingPage /> } />
+      </Routes>
+      {/* </BrowserRouter> */}
+      {/* <LandingPage/> */}
+      
     </div>
   );
 }
